2ab0e32f23fe53cdfe7497c5a366735e1eadbff6
paas <paas@cse-codegen-backend-4193087237-xf640>
2017-11-24 01:15:52 +0000

    Auto pushed by ServiceStage!

--- src/main/java/com/service/service/controller/ServiceImpl.java
+++ src/main/java/com/service/service/controller/ServiceImpl.java
@@ -4,7 +4,7 @@ package com.service.service.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import io.servicecomb.provider.pojo.RpcSchema;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoDemoCodegen", date = "2017-11-24T00:56:16.549Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoDemoCodegen", date = "2017-11-24T01:15:52.542Z")
 
 @RpcSchema(schemaId = "service")
 public class ServiceImpl implements Service{
